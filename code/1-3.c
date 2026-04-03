5dccdfeab666e189483625e8316616b2f58e9c82
goddns777 <152259531+goddns777@users.noreply.github.com>
2026-04-03 10:23:24 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-void print_reverse(int *arr, int size){
-    int *ptr = arr + size - 1;
+void read_only(const int *arr, int size){
 
-    for(int i=size-1; i>=0; i--){
-        printf("%d ", arr[i]);
+    //arr[0] = 999;
+
+    for(int i = 0; i < size; i++){
+        printf("%d ", *(arr +i));
     }
+    printf("\n");
 }
 
 int main(){
-    int nums[] = {1, 2, 3, 4, 5};
-
-    print_reverse(nums, 5);
-    printf("\n");
-
+    int nums[] = {10, 20, 30, 40, 50};
+    read_only(nums, 5);
     return 0;
 }
