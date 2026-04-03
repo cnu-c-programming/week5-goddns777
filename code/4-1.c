8b10f045f77391555afd4cef13323b2d48687dc9
goddns777 <152259531+goddns777@users.noreply.github.com>
2026-04-03 10:12:43 +0900

    Update 4-1.c

---
+++ code/4-1.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+#include <string.h>
+void echo(){
+    char buf[5];
+    scanf("%4s", buf);
+    printf("%s\n", buf);
+}
+
+int main(){
+    int x  = 42;
+    echo();
+    return 0;
+}
